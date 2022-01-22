1e0d6af4101f997016633332c215fef343089c29
Daniil <daniil.okrug@gmail.com>
2022-01-22 22:38:37 +0300

    Side menu initial

---
+++ src/components/SideMenu/SideMenu.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from 'react';
+import './Login.css'
+import { Form, Button, Container, Row, Col } from 'react-bootstrap';
+import Menu from '../../components/Menu/Menu';
+
+const SideMenu = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default SideMenu;
